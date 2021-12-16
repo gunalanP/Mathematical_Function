93964c8634a737838c968f5ae921d3a566460ea4
Gunalan Poyyamozhi <79532005+gunalanP@users.noreply.github.com>
2021-12-16 22:10:28 +0530

    maths.py
    update adition subtraction divde & Multiplication added

--- add_divide.py
+++ add_divide.py
@@ -4,11 +4,34 @@
 user_input_Value_1 = int(input('Enter The 1st Value:'))
 user_input_value_2 = int(input('Enter The 2nd Value:'))
 
-#function defintion for addition
+#function defintion for mathematical Function
 
 def add(x,y):
     added_value=x+y
     return added_value
+def multiply(x,y):
+    multi = x*y
+    return multi
+def divide(x,y):
+    divis = x/y
+    modlus=x%y
+    print(modlus)
+    return divis
+def subtract(x,y):
+    subt = x-y
+    return subt
 
-calc=add(user_input_Value_1,user_input_value_2)
-print('\nFirst Value:',user_input_Value_1,'\nSecond Value',user_input_value_2,'\nAddition Value:',calc)
+
+
+
+calc_add=add(user_input_Value_1,user_input_value_2)
+print('\nFirst Value:',user_input_Value_1,'\nSecond Value',user_input_value_2,'\nAddition Value:',calc_add)
+
+calc_multi= multiply(user_input_Value_1,user_input_value_2)
+print('\nFirst Value:',user_input_Value_1,'\nSecond Value',user_input_value_2,'\nMultiplied Value:',calc_multi)
+
+calc_divi= divide(user_input_Value_1,user_input_value_2)
+print('\nFirst Value:',user_input_Value_1,'\nSecond Value',user_input_value_2,'\nDivided Value:',calc_divi)
+
+calc_subt= subtract(user_input_Value_1,user_input_value_2)
+print('\nFirst Value:',user_input_Value_1,'\nSecond Value',user_input_value_2,'\nSubtracted Value:',calc_subt)
